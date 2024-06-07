22a5c0f4ac44f0bacb3c8f1375ff8f03c21d18da
Nishant <154457095+whynishant@users.noreply.github.com>
2024-06-07 20:12:18 +0530

    Update MyPortfolio.jsx

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -9,6 +9,7 @@ export default function MyPortfolio() {
           <h2 className="section--heading">My Portfolio</h2>
         </div>
         <div>
+          <a href="https://github.com/whynishant">
           <button className="btn btn-github">
             <svg
               xmlns="http://www.w3.org/2000/svg"
